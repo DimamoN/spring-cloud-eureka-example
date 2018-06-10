11e1c240428f9d4f9f2d782cb836d6b7dc049ac8
Dmitriy Gutman <dimamon@list.ru>
2018-06-10 15:56:25 +0300

    config server configured

--- db-service/src/main/java/com/dimamon/dbservice/NewsCacheController.java
+++ db-service/src/main/java/com/dimamon/dbservice/NewsCacheController.java
@@ -1,5 +1,7 @@
 package com.dimamon.dbservice;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,24 @@ public class NewsCacheController {
     @RequestMapping("/{articleId}")
     public String getArticle(@PathVariable("articleId") String id){
         return "Article: " + id;
+    }
+
+
+    // Endpoints to get properties from configuration server //
+
+    @RequestMapping("/property1")
+    public String getParamOne(@Value("${db.param.one}") String param){
+        return param;
+    }
+
+    @RequestMapping("/property2")
+    public String getParamTwo(@Value("${db.param.two}") String param){
+        return param;
+    }
+
+    @RequestMapping("/property3")
+    public String getParamThree(@Value("${param.one}") String param){
+        return param;
     }
 
 }
--- news-service/src/main/java/com/dimamon/newsservice/NewsSearchController.java
+++ news-service/src/main/java/com/dimamon/newsservice/NewsSearchController.java
@@ -2,12 +2,14 @@ package com.dimamon.newsservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
+@ConfigurationProperties // adds support for properties from spring-config-server
 @RequestMapping("/rest/search")
 public class NewsSearchController {
 
@@ -24,4 +26,31 @@ public class NewsSearchController {
         String text = " [found by " +  port + "]";
         return restTemplate.getForObject("http://db-service/rest/db/" + articleName, String.class) + text;
     }
+
+
+    // Endpoint to get user.home param value //
+
+    @RequestMapping("/userhome")
+    public String getUserhome(@Value("${user.home}") String userhome){
+        return userhome;
+    }
+
+
+    // Endpoints to get properties from configuration server //
+
+    @RequestMapping("/property1")
+    public String getParamOne(@Value("${news.param.one}") String param){
+        return param;
+    }
+
+    @RequestMapping("/property2")
+    public String getParamTwo(@Value("${news.param.two}") String param){
+        return param;
+    }
+
+    @RequestMapping("/property3")
+    public String getParamThree(@Value("${param.one}") String param){
+        return param;
+    }
+
 }
